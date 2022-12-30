08ee559eb8c2a3b28f6e776d7c7e6678fa73fd87
Jim McAliece <jim.mcaliece@crayon.com>
2022-12-29 21:39:49 -0500

    More Comments More

--- azure-function/dotnet/GetBusDataFunction.cs
+++ azure-function/dotnet/GetBusDataFunction.cs
@@ -3,7 +3,7 @@ namespace GetBusData;
 public class GetBusDataFunction
 {
     private readonly IBusDataManagerService _busDataManagerService;
-    // Comment
+    // Comment More Comments
     public GetBusDataFunction(
         IBusDataManagerService busDataManagerService) =>
         _busDataManagerService = busDataManagerService;
